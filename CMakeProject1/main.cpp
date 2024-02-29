836a39cec8cea196d448b94bda869a9cc04ea15c
SpicyRicecaker <awesomenessisgolden@gmail.com>
2024-02-29 01:58:04 -0800

    tilt

--- CMakeProject1/main.cpp
+++ CMakeProject1/main.cpp
@@ -1,9 +1,162 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <format>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+// WIP 
+// the most scuffed readfile function I have ever seen in my life
+string readFile(const char* path) {
+  ifstream file(path);
+
+  if (!file.is_open()) {
+    cerr << "Could not open the file - '" << path << "'" << endl;
+  }
+  
+  string output;
+  string s;
+  while (getline(file, s)) {
+    // Process or print the line
+    for (auto c : s) {
+      output.push_back(c);
+    }
+    output.push_back('\n');
+  }
+  cout << "running read file" << endl;
+  cout << output << endl;
+  cout << "done running read file" << endl;
+
+  file.close();
+  return output;
+}
+
 int main() {
-  cout << "hello world" << endl;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Andy Li Graphics", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  const string debugPathPrefix = "../../../../CMakeProject1";
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+  };
+
+  // vertex buffer
+  unsigned int VAO, VBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
+  // vertex attributes (vertex buffer description)
+  // specify vertex attributes?
+  // copy actual data over
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  string vertexShaderSource = readFile(format("{}/shaders/pattern.vert", debugPathPrefix).c_str());
+  const GLchar* vertexShaderSourceRef = vertexShaderSource.c_str();
+  glShaderSource(vertexShader, 1, &vertexShaderSourceRef, NULL);
+  glCompileShader(vertexShader);
+
+  {
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+  }
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  string fragmentShaderSource = readFile(format("{}/shaders/pattern.frag", debugPathPrefix).c_str());
+  const GLchar* fragmentShaderSourceRef = fragmentShaderSource.c_str();
+  glShaderSource(fragmentShader, 1, &fragmentShaderSourceRef, NULL);
+  glCompileShader(fragmentShader);
+
+  {
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+  }
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+
+    {
+      glClearColor(0.6f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      // render pass
+      glUseProgram(shaderProgram);
+      glBindVertexArray(VAO);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
 
   return 0;
 }
--- CMakeProject1/shaders/pattern.vert
+++ CMakeProject1/shaders/pattern.vert
@@ -1,63 +1,8 @@
-// make this 120 for the mac:
-#version 460 compatibility
+#version 330 core
 
-#define M_PI 3.1415926535897932384626433832795
+layout (location = 0) in vec3 aPos;
 
-// out variables to be interpolated in the rasterizer and sent to each fragment shader:
-out  vec3  vN;	  // normal vector
-out  vec3  vL;	  // vector from point to light
-out  vec3  vE;	  // vector from point to eye
-out  vec2  vST;	  // (s,t) texture coordinates
-out  vec3  vMCposition;
-
-// where the light is:
-const vec3 LightPosition = vec3(  0., 5., 5. );
-
-// z = A\cos(2\piBr + C)e^{-Dr}
-// uA : amplitude
-// uB : period
-// uC : phase shift
-// uD : decay rate
-// r  : radius = sqrt(x^2 + y^2)
-
-// how to get the tangent vector at each point?
-// f(x,y) = z = ...
-// f(x,y) - z = 0
-// \nabla f(x,y,z) = (df/dx, df/dy, df/dz)
-// \nabla f(x,y,z) = (df/dx, df/dy, -1)
-// \nabla f(x,y,z) = (df/dr * dr/dx, df/dr * dr/dy, -1)
-
-uniform float uA, uB, uC, uD;
-
-void
-main()
+void main()
 {
-	// // interpolated s & t coordinates?
-	// vST = gl_MultiTexCoord0.st;
-
-	vec4 vert = gl_Vertex;
-	// float r = sqrt(pow(vert.x, 2) + pow(vert.y, 2));
-	// vert.z = uA * cos(2. * M_PI * uB * r + uC) * exp(-uD * r);
-
-	// float df_dr = -uA * sin(2. * M_PI * uB * r + uC) * 2. * M_PI * uB * exp(-uD * r) 
-	//               + uA * cos(2. * M_PI * uB * r + uC) * exp(-uD * r);
-
-	// float dr_dx = vert.x * sqrt(pow(vert.x, 2) + pow(vert.y, 2));
-	// float dr_dy = vert.y * sqrt(pow(vert.x, 2) + pow(vert.y, 2));
-
-  // 	// why do we need this
-	// vec4 ECposition = gl_ModelViewMatrix * vert;
-	// // vector normal to current vertex?
-	// // gl_NormalMatrix ensures that normals are transformed correctly normals
-	// // are direction vectors while vertices are position vectors, so they need
-	// // to be treated differently
-	// vN = normalize( gl_NormalMatrix * vec3(df_dr * dr_dx, df_dr * dr_dy, 1.));  // normal vector
-	// // vector from light to point
-	// vL = LightPosition - ECposition.xyz;	    // vector from the point to the light position
-	// vE = vec3( 0., 0., 0. ) - ECposition.xyz;       // vector from the point to the eye position
-	// // add coordinates for later
-	// vMCposition = vert.xyz;
-	// // map vertex position from world space to 2d screen space (monitor resolution) basically
-	// gl_Position = gl_ModelViewProjectionMatrix * vert;
-	gl_Position = gl_ModelViewProjectionMatrix * vert;
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
